230737407237ce35694d21e48007b468624694f5
Mohit Bansal <workmohit17@gmail.com>
2025-07-09 12:21:07 +0530

    Initial Commit

---
+++ src/main/java/com/example/bansalmotors/Bansal/Motors/BansalMotorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bansalmotors.Bansal.Motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BansalMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BansalMotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bansalmotors/Bansal/Motors/BansalMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bansalmotors.Bansal.Motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansalMotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
