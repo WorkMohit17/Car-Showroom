dfeeb254520a9e3550e2a247d503eaf11bdece0a
Mohit Bansal <workmohit17@gmail.com>
2025-07-13 16:44:22 +0530

    added swaggerConfig

---
+++ src/main/java/com/example/bansalmotors/Bansal/Motors/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.bansalmotors.Bansal.Motors.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bansal Motors API")
+                        .version("1.0")
+                        .description("REST API for managing car showroom operations."));
+    }
+}
